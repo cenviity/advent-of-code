0168b931fd13982624a0463565067c030383502d
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-02 11:14:53 +1100

    Extract v2 method to find first and last digits in each line
    Taking into account numbers written as words this time.

--- 2023/01_trebuchet/aoc_2023_01.py
+++ 2023/01_trebuchet/aoc_2023_01.py
@@ -26,8 +26,14 @@ def is_digit(char):
 
 
 def solve_part2(data):
-    calibration_values = []
+    calibration_values = [
+        find_first_and_last_digits_with_numeric_words(line) for line in data
+    ]
 
+    return sum(calibration_values)
+
+
+def find_first_and_last_digits_with_numeric_words(line):
     number_words = [
         ("one", "1"),
         ("two", "2"),
@@ -40,65 +46,61 @@ def solve_part2(data):
         ("nine", "9"),
     ]
 
-    for line in data:
-        # Implement `find` for first_digit
+    # Implement `find` for first_digit
 
-        number_word_indices = [
-            (number_word[0], line.find(number_word[0]))
-            for number_word in number_words
-            if number_word[0] in line
-        ]
+    number_word_indices = [
+        (number_word[0], line.find(number_word[0]))
+        for number_word in number_words
+        if number_word[0] in line
+    ]
 
-        for i, number_word_index in enumerate(number_word_indices):
-            for r in number_words:
-                number_word_indices[i] = (
-                    number_word_indices[i][0].replace(*r),
-                    number_word_indices[i][1],
-                )
+    for i, number_word_index in enumerate(number_word_indices):
+        for r in number_words:
+            number_word_indices[i] = (
+                number_word_indices[i][0].replace(*r),
+                number_word_indices[i][1],
+            )
 
-        digit_indices = [
-            (number_word[1], line.find(number_word[1]))
-            for number_word in number_words
-            if number_word[1] in line
-        ]
+    digit_indices = [
+        (number_word[1], line.find(number_word[1]))
+        for number_word in number_words
+        if number_word[1] in line
+    ]
 
-        combined_indices = number_word_indices + digit_indices
+    combined_indices = number_word_indices + digit_indices
 
-        sorted_combined_indices = sorted(combined_indices, key=lambda pair: pair[1])
+    sorted_combined_indices = sorted(combined_indices, key=lambda pair: pair[1])
 
-        first_digit = sorted_combined_indices[0][0]
+    first_digit = sorted_combined_indices[0][0]
 
-        # Implement `rfind` instead for last_digit
+    # Implement `rfind` instead for last_digit
 
-        number_word_indices = [
-            (number_word[0], line.rfind(number_word[0]))
-            for number_word in number_words
-            if number_word[0] in line
-        ]
+    number_word_indices = [
+        (number_word[0], line.rfind(number_word[0]))
+        for number_word in number_words
+        if number_word[0] in line
+    ]
 
-        for i, number_word_index in enumerate(number_word_indices):
-            for r in number_words:
-                number_word_indices[i] = (
-                    number_word_indices[i][0].replace(*r),
-                    number_word_indices[i][1],
-                )
+    for i, number_word_index in enumerate(number_word_indices):
+        for r in number_words:
+            number_word_indices[i] = (
+                number_word_indices[i][0].replace(*r),
+                number_word_indices[i][1],
+            )
 
-        digit_indices = [
-            (number_word[1], line.rfind(number_word[1]))
-            for number_word in number_words
-            if number_word[1] in line
-        ]
+    digit_indices = [
+        (number_word[1], line.rfind(number_word[1]))
+        for number_word in number_words
+        if number_word[1] in line
+    ]
 
-        combined_indices = number_word_indices + digit_indices
+    combined_indices = number_word_indices + digit_indices
 
-        sorted_combined_indices = sorted(combined_indices, key=lambda pair: pair[1])
+    sorted_combined_indices = sorted(combined_indices, key=lambda pair: pair[1])
 
-        last_digit = sorted_combined_indices[-1][0]
+    last_digit = sorted_combined_indices[-1][0]
 
-        calibration_value = int(first_digit + last_digit)
-        calibration_values.append(calibration_value)
-
-    return sum(calibration_values)
+    return int(first_digit + last_digit)
 
 
 def solve_day(puzzle_input):
