bbfa9b4e996ac900be18c665cda7bfa196b3f3d2
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-05 13:58:50 +1100

    Solve part 1

--- 2023/03_gear_ratios/aoc_2023_03.py
+++ 2023/03_gear_ratios/aoc_2023_03.py
@@ -1,4 +1,6 @@
 import pathlib
+import re
+import string
 import sys
 
 
@@ -7,7 +9,45 @@ def parse_input(puzzle_input):
 
 
 def solve_part1(data):
-    pass
+    part_number_total = 0
+
+    for line_number, line in enumerate(data):
+        number_matches = find_number_matches(line)
+
+        for number_match in number_matches:
+            match_start_column = number_match.start()
+            match_end_column = number_match.end() - 1
+            number = int(number_match.group())
+
+            if is_adjacent_to_symbol(
+                data, line_number, match_start_column, match_end_column
+            ):
+                part_number_total += number
+
+    return part_number_total
+
+
+def find_number_matches(line):
+    return re.finditer(r"\d+", line)
+
+
+def is_adjacent_to_symbol(data, row_number, start_column, end_column):
+    surrounding_coords = [
+        (row_number + row_offset, i)
+        for i in range(start_column - 1, end_column + 2)
+        for row_offset in [-1, 1]
+    ] + [(row_number, i) for i in [start_column - 1, end_column + 1]]
+
+    return any(map(lambda coords: is_symbol(data, coords), surrounding_coords))
+
+
+def is_symbol(data, coords):
+    row, column = coords
+
+    try:
+        return data[row][column] not in ("." + string.digits)
+    except IndexError:
+        return False
 
 
 def solve_part2(data):
