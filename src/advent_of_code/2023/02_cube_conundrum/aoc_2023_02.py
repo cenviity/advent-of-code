3549e102e2d6103ee1e0e012e7745730339287dd
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-11 17:28:01 +1100

    Simplify logic for deriving `CubeSet`s

--- src/advent_of_code/2023/02_cube_conundrum/aoc_2023_02.py
+++ src/advent_of_code/2023/02_cube_conundrum/aoc_2023_02.py
@@ -1,6 +1,5 @@
 import pathlib
 import sys
-from collections import defaultdict
 from typing import Iterator, NamedTuple, NewType
 
 
@@ -34,32 +33,31 @@ def extract_gameid_and_cubesets(line: str) -> tuple[GameId, list[CubeSet]]:
     cube_draws: list[str] = [
         cube_draw.strip() for cube_draw in all_cube_draws.split(";")
     ]
-    processed_cube_draws: list[CubeSet] = list(map(process_cube_draw, cube_draws))
+    processed_cube_draws: list[CubeSet] = list(map(extract_cubeset, cube_draws))
 
     return game_id, processed_cube_draws
 
 
-def process_cube_draw(cube_draw: str) -> CubeSet:
+def extract_cubeset(cube_draw: str) -> CubeSet:
     cube_sets: list[str] = [cube_set.strip() for cube_set in cube_draw.split(",")]
 
-    processed_cube_draw: defaultdict[str, int] = defaultdict(int)
+    processed_cube_draw: dict[str, int] = dict(
+        extract_cube_colour_and_count(cube_set) for cube_set in cube_sets
+    )
 
-    for cube_set in cube_sets:
-        cube_count: str
-        cube_colour: str
-        cube_count, cube_colour = cube_set.split(" ")
-
-        processed_cube_draw[cube_colour] = int(cube_count)
-
-    return convert_dict_to_cubeset(processed_cube_draw)
+    return convert_to_cubeset(processed_cube_draw)
 
 
-def convert_dict_to_cubeset(cube_draw: defaultdict[str, int]) -> CubeSet:
-    red_cubes: int = cube_draw["red"]
-    green_cubes: int = cube_draw["green"]
-    blue_cubes: int = cube_draw["blue"]
+def extract_cube_colour_and_count(cube_set: str) -> tuple[str, int]:
+    cube_count: str
+    cube_colour: str
+    cube_count, cube_colour = cube_set.split(" ")
 
-    return CubeSet(red_cubes, green_cubes, blue_cubes)
+    return cube_colour, int(cube_count)
+
+
+def convert_to_cubeset(cube_draw: dict[str, int]) -> CubeSet:
+    return CubeSet(**cube_draw)
 
 
 def solve_part1(games: list[tuple[GameId, list[CubeSet]]]) -> int:
