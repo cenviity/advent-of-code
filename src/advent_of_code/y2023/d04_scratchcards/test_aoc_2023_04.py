a276de9304a3ba93f5ddd36dd8b35a4793cc8c23
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-12 15:29:08 +1100

    Add example and tests for part 1

--- src/advent_of_code/y2023/d04_scratchcards/test_aoc_2023_04.py
+++ src/advent_of_code/y2023/d04_scratchcards/test_aoc_2023_04.py
@@ -6,33 +6,40 @@ PUZZLE_DIR: pathlib.Path = pathlib.Path(__file__).parent
 
 
 @pytest.fixture
-def example1() -> None:
+def example1() -> aoc.Sequence[aoc.Card]:
     puzzle_input: str = (PUZZLE_DIR / "example1.txt").read_text().strip()
     return aoc.parse_input(puzzle_input)
 
 
 @pytest.fixture
-def example2() -> None:
+def example2() -> aoc.Sequence[aoc.Card]:
     puzzle_input: str = (PUZZLE_DIR / "example2.txt").read_text().strip()
     return aoc.parse_input(puzzle_input)
 
 
 @pytest.fixture
-def actual_input() -> None:
+def actual_input() -> aoc.Sequence[aoc.Card]:
     puzzle_input: str = (PUZZLE_DIR / "input.txt").read_text().strip()
     return aoc.parse_input(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1: None) -> None:
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert example1 == [
+        aoc.Card(1, [41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53]),
+        aoc.Card(2, [13, 32, 20, 16, 61], [61, 30, 68, 82, 17, 32, 24, 19]),
+        aoc.Card(3, [1, 21, 53, 59, 44], [69, 82, 63, 72, 16, 21, 14, 1]),
+        aoc.Card(4, [41, 92, 73, 84, 69], [59, 84, 76, 51, 58, 5, 54, 83]),
+        aoc.Card(5, [87, 83, 26, 28, 32], [88, 30, 70, 12, 93, 22, 82, 36]),
+        aoc.Card(6, [31, 18, 13, 56, 72], [74, 77, 10, 23, 35, 67, 36, 11]),
+    ]
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1: None) -> None:
     """Test part 1 on example input."""
-    assert aoc.solve_part1(example1) == ...
+    assert aoc.solve_part1(example1) == 13
 
 
 @pytest.mark.skip(reason="Not implemented")
