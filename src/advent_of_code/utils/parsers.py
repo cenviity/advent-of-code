1d31860f392b03891e9ef3d5fd65b54d45a8321c
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-14 18:04:31 +1100

    Add `lexeme` helper function

--- src/advent_of_code/utils/parsers.py
+++ src/advent_of_code/utils/parsers.py
@@ -8,5 +8,9 @@ p_optional_whitespace: Parser = regex(r"\s*")
 p_number: Parser = regex(r"\d+").map(int) << p_optional_whitespace
 
 
+def lexeme(parser: Parser) -> Parser:
+    return parser << p_optional_whitespace
+
+
 def symbol(text: str) -> Parser:
     return string(text) << p_optional_whitespace
