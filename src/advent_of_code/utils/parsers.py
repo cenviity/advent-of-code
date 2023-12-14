da999538dcadd7c92ec27aa9c742bc77a6edaa9c
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-14 18:03:58 +1100

    Make whitespace optional following a string

--- src/advent_of_code/utils/parsers.py
+++ src/advent_of_code/utils/parsers.py
@@ -2,11 +2,11 @@
 # pyright: reportUnknownMemberType=false
 # pyright: reportUnknownVariableType=false
 
-from parsy import Parser, regex, string, whitespace
+from parsy import Parser, regex, string
 
 p_optional_whitespace: Parser = regex(r"\s*")
 p_number: Parser = regex(r"\d+").map(int) << p_optional_whitespace
 
 
 def symbol(text: str) -> Parser:
-    return string(text) << whitespace
+    return string(text) << p_optional_whitespace
