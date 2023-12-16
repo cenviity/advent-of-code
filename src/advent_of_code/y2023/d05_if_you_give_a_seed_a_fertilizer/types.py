3a0b61f24ed2ae2c196f6d52720684dc67f94ef0
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-16 22:24:03 +1100

    Simplify logic by merging `Correspondence` into `MapLine`

--- src/advent_of_code/y2023/d05_if_you_give_a_seed_a_fertilizer/types.py
+++ src/advent_of_code/y2023/d05_if_you_give_a_seed_a_fertilizer/types.py
@@ -3,12 +3,6 @@ from dataclasses import dataclass
 from enum import Enum
 
 
-@dataclass
-class Correspondence:
-    source: Sequence[int]
-    destination: Sequence[int]
-
-
 Category = Enum(
     "Category",
     [
@@ -25,39 +19,40 @@ Category = Enum(
 
 
 @dataclass
-class MapLine:
+class Correspondence:
     destination_start: int
     source_start: int
     range: int
 
     @property
-    def correspondence(self) -> Correspondence:
-        return Correspondence(
-            range(self.source_start, self.source_start + self.range),
-            range(self.destination_start, self.destination_start + self.range),
-        )
+    def sources(self) -> Sequence[int]:
+        return range(self.source_start, self.source_start + self.range)
+
+    @property
+    def destinations(self) -> Sequence[int]:
+        return range(self.destination_start, self.destination_start + self.range)
 
 
 @dataclass
 class Map:
     source_category: Category
     destination_category: Category
-    lines: Sequence[MapLine]
-
-    @property
-    def correspondences(self) -> Sequence[Correspondence]:
-        return [line.correspondence for line in self.lines]
+    correspondences: Sequence[Correspondence]
 
     def destination(self, src: int) -> int:
         for correspondence in self.correspondences:
-            if src in correspondence.source:
-                return src - correspondence.source[0] + correspondence.destination[0]
+            if src in correspondence.sources:
+                return (
+                    src - correspondence.source_start + correspondence.destination_start
+                )
         return src
 
     def source(self, dst: int) -> int:
         for correspondence in self.correspondences:
-            if dst in correspondence.source:
-                return dst - correspondence.source[0] + correspondence.destination[0]
+            if dst in correspondence.destinations:
+                return (
+                    dst - correspondence.destination_start + correspondence.source_start
+                )
         return dst
 
 
